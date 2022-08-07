b05d463a7134b6de44a9b9d9eec1ae3d6955aa52
kaishajri <rafik_alex53@outlook.fr>
2022-08-07 11:38:31 +0100

    s1

--- Source/Views/bonTravailList.py
+++ Source/Views/bonTravailList.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QMessageBox
 from .BonTravail import Ui_Dialog as Bontravail_UI
 from .BonTravailModify import Ui_Dialog as BonTravail_Modif_UI
 from Models.BonTravailServices import getBonTravailList,deleteBonTravail
+
 class Ui_Dialog(object):
     def getSelectedRow(self):
         rows=[]
