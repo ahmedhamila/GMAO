f622c87e28efdca693f6605d41a48f4a004e01ee
Ahmed <csahmed17@gmail.com>
2022-08-07 13:24:07 +0100

    DB

--- Source/gmao_db.sql
+++ Source/gmao_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 27, 2022 at 11:42 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Host: localhost:3306
+-- Generation Time: Aug 07, 2022 at 02:23 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -73,16 +73,21 @@ CREATE TABLE `bon_travail` (
   `Section` varchar(25) NOT NULL,
   `DateLiberation` date NOT NULL,
   `type` enum('Correctif','Preventif') NOT NULL,
-  `CodeEquipement` varchar(25) NOT NULL
+  `CodeEquipement` varchar(25) NOT NULL,
+  `RefDIM` varchar(50) NOT NULL,
+  `Frequence` varchar(50) DEFAULT NULL,
+  `Active` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `bon_travail`
 --
 
-INSERT INTO `bon_travail` (`Id`, `Matricule_RM`, `Matricule_AM`, `Description`, `Section`, `DateLiberation`, `type`, `CodeEquipement`) VALUES
-(5, 'AAA00001', 'AAA00009', 'a:kjfnakfkanf\nahdakdkabdd\nddddddddddd', 'C', '2022-07-26', 'Correctif', 'EEE00001'),
-(6, 'AAA00001', 'AAA00009', 'a:kjfnakfkanf\nakfjbkafbkabfka', 'B', '2022-07-26', 'Correctif', 'EEE00002');
+INSERT INTO `bon_travail` (`Id`, `Matricule_RM`, `Matricule_AM`, `Description`, `Section`, `DateLiberation`, `type`, `CodeEquipement`, `RefDIM`, `Frequence`, `Active`) VALUES
+(5, 'AAA00001', 'AAA00009', 'a:kjfnakfkanf\nahdakdkabdd\nddddddddddd', 'C', '2022-07-26', 'Correctif', 'EEE00001', '1', NULL, 0),
+(6, 'AAA00001', 'AAA00009', 'a:kjfnakfkanf\nakfjbkafbkabfka', 'B', '2022-07-26', 'Correctif', 'EEE00002', '2', NULL, 0),
+(15, 'AAA00001', 'AAA00009', 'ezsrdtfyughinjlk,af65', 'T', '2022-07-27', 'Correctif', 'EEE00001', '8', 'NULL', 0),
+(16, 'AAA00001', 'AAA00009', 'oooh!', 'op', '2022-07-27', 'Preventif', 'EEE00002', '', '112', 0);
 
 -- --------------------------------------------------------
 
@@ -106,6 +111,13 @@ CREATE TABLE `chaine_production` (
   `NbEquipement` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `chaine_production`
+--
+
+INSERT INTO `chaine_production` (`RefChaine`, `NbEquipement`) VALUES
+('CCC1', 50);
+
 -- --------------------------------------------------------
 
 --
@@ -113,7 +125,7 @@ CREATE TABLE `chaine_production` (
 --
 
 CREATE TABLE `demande_intervention` (
-  `Id` varchar(25) NOT NULL,
+  `Id` int(25) NOT NULL,
   `Matricule_RCP` varchar(50) NOT NULL,
   `Matricule_RM` varchar(50) NOT NULL,
   `CodeEquipement` varchar(25) NOT NULL,
@@ -122,6 +134,16 @@ CREATE TABLE `demande_intervention` (
   `Motif` enum('ArretComplet','AnomaliePouvantEntrainerUnePanne','','') NOT NULL,
   `Description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `demande_intervention`
+--
+
+INSERT INTO `demande_intervention` (`Id`, `Matricule_RCP`, `Matricule_RM`, `CodeEquipement`, `Section`, `DateLiberation`, `Motif`, `Description`) VALUES
+(1, 'AAA00003', 'AAA00001', 'EEE00002', '2', '2022-08-08', 'ArretComplet', 'ajgvdkahbdabdibaidad'),
+(2, 'AAA00003', 'AAA00001', 'EEE00002', '2', '2022-08-07', 'AnomaliePouvantEntrainerUnePanne', 'adljalkdbabd'),
+(3, 'AAA00003', 'AAA00001', 'EEE00003', '2', '2022-08-07', 'ArretComplet', 'akhfbkajhbfkabfabf'),
+(4, 'AAA00003', 'AAA00001', 'EEE00002', '3', '2022-08-07', 'ArretComplet', 'dafjkhakhfjkahbfjba');
 
 -- --------------------------------------------------------
 
@@ -194,6 +216,13 @@ CREATE TABLE `responsable_chaine_production` (
   `RefChaine` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `responsable_chaine_production`
+--
+
+INSERT INTO `responsable_chaine_production` (`Matricule`, `Nom`, `Prenom`, `RefChaine`) VALUES
+('AAA00003', 'Touhemi', 'boubaker', 'CCC1');
+
 -- --------------------------------------------------------
 
 --
@@ -212,7 +241,8 @@ CREATE TABLE `responsable_maintenance` (
 --
 
 INSERT INTO `responsable_maintenance` (`Matricule`, `Nom`, `Prenom`, `RefChaine`) VALUES
-('AAA00001', 'Hamila', 'Ahmed', '1');
+('AAA00001', 'Hamila', 'Ahmed', '1'),
+('AAA00005', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -235,7 +265,7 @@ CREATE TABLE `responsable_production` (
 CREATE TABLE `users` (
   `Matricule` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
-  `Role` enum('ResponsableProduction','ResponsableChaineProduction','ResponsableMaintenance','AgentMaintenance','Administrateur') NOT NULL
+  `Role` enum('ResponsableProduction','ResponsableChaineProduction','ResponsableMaintenance','AgentMaintenance','Administrateur','Magasinier') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -244,7 +274,29 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`Matricule`, `Password`, `Role`) VALUES
 ('AAA00001', 'Hamila', 'ResponsableMaintenance'),
-('AAA00002', 'Hajri', 'Administrateur');
+('AAA00002', 'Hajri', 'Administrateur'),
+('AAA00003', 'Gaston', 'ResponsableChaineProduction'),
+('AAA00005', 'azerty', 'ResponsableMaintenance');
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `User_Insert` AFTER INSERT ON `users` FOR EACH ROW BEGIN
+    IF (NEW.Role = "ResponsableMaintenance") THEN
+    	insert into responsable_maintenance(matricule) VALUES (NEW.matricule);
+    ELSEIF (NEW.Role = "ResponsableChaineProduction") THEN
+    	insert into responsable_chaine_production(matricule) VALUES (NEW.matricule);
+    ELSEIF (NEW.Role = "AgentMaintenance") THEN
+    	insert into agent_maintenance(matricule) VALUES (NEW.matricule);
+    ELSEIF (NEW.Role = "ResponsableProduction") THEN
+    	insert into responsable_production(matricule) VALUES (NEW.matricule);
+    ELSEIF (NEW.Role = "Magasinier") THEN
+    	insert into magasinier(matricule) VALUES (NEW.matricule);
+    End IF;
+END
+$$
+DELIMITER ;
 
 --
 -- Indexes for dumped tables
@@ -354,7 +406,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bon_travail`
 --
 ALTER TABLE `bon_travail`
-  MODIFY `Id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `Id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `demande_intervention`
+--
+ALTER TABLE `demande_intervention`
+  MODIFY `Id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `piece_rechange_bon_approvisionnement`
